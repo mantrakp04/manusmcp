470dd02822dd1da6a481cf5f8f38b1b84c0077aa
BarrelOfLube <mantrakp04@gmail.com>
2025-03-18 12:24:44 -0700

    Add creation of /mnt/data directory in start.sh and comment out flowise start command

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 . /root/.nvm/nvm.sh
+mkdir -p /mnt/data
 mkdir $LOG_PATH && mkdir $SECRETKEY_PATH && mkdir $DATABASE_PATH && mkdir $BLOB_STORAGE_PATH
 cd /app/.runtime/unstructured-api && source .venv/bin/activate && make run-web-app &
 sleep 10
 cd /app
 uv run --project ./.runtime manusmcp &
 uv run --project ./.runtime litellm --config ./.runtime/litellm.yml &
-bunx flowise@latest start
+# bunx flowise@latest start
